0bd872653d3bee6fe7974b19b0220a650a41e3ca
Talerchik Alexey <talerchik.al@gmail.com>
2022-10-06 19:26:29 +0300

    fixed: new array style

--- Labyrinth/index.js
+++ Labyrinth/index.js
@@ -9,16 +9,14 @@ function isPath(arr) {
 
   var queue = [];
 
-  // Insert the top right corner.
-  queue.push([0, 0]);
+  queue.push(GetStartPoint(arr, 'S'));
 
-  // Until queue is empty
   while (queue.length > 0) {
     var currentNode = queue[0];
     queue.shift();
 
     // Mark as visited
-    arr[currentNode[0]][currentNode[1]] = -1;
+    arr[currentNode[0]][currentNode[1]] = '#';
 
     // Check all four directions
     for (var i = 0; i < 4; i++) {
@@ -27,8 +25,8 @@ function isPath(arr) {
       var b = currentNode[1] + direction[i][1];
 
       // Not blocked and valid
-      if (a >= 0 && b >= 0 && a < row && b < col && arr[a][b] != -1) {
-        if (arr[a][b] == 1) return true;
+      if (a >= 0 && b >= 0 && a < row && b < col && arr[a][b] != '#') {
+        if (arr[a][b] == 'E') return true;
         queue.push([a, b]);
       }
     }
@@ -36,12 +34,22 @@ function isPath(arr) {
   return false;
 }
 
+function GetStartPoint(arr, symbol) {
+  for (let row = 0; row < arr.length; row++) {
+    for (let col = 0; col < arr[row].length; col++) {
+      if (arr[row][col] == symbol) {
+        return [row, col]
+      }
+    }
+  }
+}
+
 var arr = [
-  [0, 0, 0, -1, 0],
-  [-1, 0, 0, -1, -1],
-  [0, 0, 0, -1, 0],
-  [-1, 0, 0, 0, 1],
-  [0, 0, -1, -1, -1],
+  ['#', 'S', '#', '#', '#'],
+  ['#', '.', '#', '.', 'E'],
+  ['#', '.', '#', '.', '#'],
+  ['#', '.', '.', '.', '#'],
+  ['#', '#', '#', '#', '#'],
 ];
 
 if (isPath(arr)) {
