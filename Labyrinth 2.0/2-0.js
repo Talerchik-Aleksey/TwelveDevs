794038f94cf6ed8e3cd4e5bce93bf19348ba8fb5
Talerchik Alexey <talerchik@protonmail.com>
2022-10-08 20:58:51 +0300

    Why?

--- Labyrinth 2.0/2-0.js
+++ Labyrinth 2.0/2-0.js
@@ -1,140 +1,105 @@
 size = [5, 5]
 
 var arr = [
-  ["#", "#", "#", "#", "#"],
   ["#", "S", "#", "#", "#"],
+  ["#", ".", "#", ".", "E"],
   ["#", ".", ".", ".", "#"],
-  ["#", ".", "#", ".", "#"],
-  ["#", "#", "#", "E", "#"],
+  ["#", ".", ".", ".", "#"],
+  ["#", "#", "#", "#", "#"],
 ];
 
-const path = SearchPath(arr, size);
-console.log(path);
+const direction = [
+  [0, 1], [0, -1],
+  [1, 0], [-1, 0],
+];
 
-function SearchPath(arr, size) {
-  const startPoint = FindFirstCoincidenceInMatrix(arr);
-  if (startPoint == null) throw new Error(`NullStartPoint ${arr}`);
+console.log(SearchPath());
 
-  var queueForCheaking = [];
-  queueForCheaking = SearchFreePath(arr, startPoint, queueForCheaking, size);
-  const result = FindPath(arr, size, queueForCheaking);
-  console.log(result);
-  const path = getPath(result, size);
-  return path;
-}
+function SearchPath() {
+  const startNode = FindFirstCoincidenceInMatrix(arr);
+  let currentNode = startNode;
+  let Path = [];
+  stopFlag = false;
 
-function getPath(arr, size) {
-  const startPoint = FindFirstCoincidenceInMatrix(arr);
-  if (startPoint == null) throw new Error(`NullStartPoint ${arr}`);
-  let path = [];
-  let queueForCheaking = []
-  queueForCheaking.push(startPoint);
+  while (arr[currentNode[0]][currentNode[1]] != 'E') {
+    if (arr[currentNode[0]][currentNode[1]] != 'S') {
+      arr[currentNode[0]][currentNode[1]] = '1';
+    }
 
-  while (queueForCheaking.length > 0) {
-    const currentPoint = queueForCheaking[0];
-    queueForCheaking.shift();
+    for (var i = 0; i < 4; i++) {
+      var rowNumber = currentNode[0] + direction[i][0];
+      var colNumber = currentNode[1] + direction[i][1];
 
-    arr[currentPoint[0]][currentPoint[1]] = '2';
+      if (isNotBlocked(rowNumber, colNumber, size) && isValid(arr, rowNumber, colNumber)) {
 
-    const direction = [
-      [0, 1], [0, -1],
-      [1, 0], [-1, 0],
-    ];
-    const pointSidesNumber = 4;
-
-    for (var i = 0; i < pointSidesNumber; i++) {
-      var rowNumber = currentPoint[0] + direction[i][0];
-      var colNumber = currentPoint[1] + direction[i][1];
-
-      if (isNotBlocked(rowNumber, colNumber, size) && isValid(arr, rowNumber, colNumber, ["#", "S", "2", "E"])) {
-        queueForCheaking.push([rowNumber, colNumber]);
         switch (i) {
           case 0:
-            path.push('r')
+            Path.push('r')
             break;
           case 1:
-            path.push('l')
+            Path.push('l')
             break;
           case 2:
-            path.push('d')
+            Path.push('d')
             break;
           case 3:
-            path.push('u')
-            break;
-          default:
+            Path.push('u')
             break;
         }
-        if (isDoor(arr, [currentPoint[0], currentPoint[1]], size))
-          break;
+        i = 4; // Stop For
+        currentNode = [rowNumber, colNumber];
       }
+
+      if (isStop(arr, currentNode) && arr[currentNode[0]][currentNode[1]] != "E") {
+        arr[currentNode[0]][currentNode[1]] = '#';
+        ClearVisited();
+        SearchPath();
+      }
+
+      if (arr[rowNumber][colNumber] == "E") break;
     }
   }
-  return path;
+  return Path;
 }
 
-function FindPath(arr, size, queueForCheaking) {
-  while (isDoor(arr, queueForCheaking[0], size) == false) {
-    const currentPoint = queueForCheaking[0];
-    queueForCheaking.shift();
-    arr[currentPoint[0]][currentPoint[1]] = '1';
-    if (SearchFreePath(arr, currentPoint, queueForCheaking, size) == false) break;
+function ClearVisited() {
+  const [rowSize, colSize] = size;
+  for (let row = 0; row < rowSize; row++) {
+    for (let col = 0; col < colSize; col++) {
+      if (arr[row][col] == '1') {
+        arr[row][col] = '.'
+      }
+    }
   }
   return arr;
 }
 
-function isDoor(arr, currentPoint, size) {
-  const direction = [
-    [0, 1], [0, -1],
-    [1, 0], [-1, 0],
-  ];
-  const pointSidesNumber = 4;
+function isStop(arr, currentNode) {
+  result = true;
 
-  for (var i = 0; i < pointSidesNumber; i++) {
-    var rowNumber = currentPoint[0] + direction[i][0];
-    var colNumber = currentPoint[1] + direction[i][1];
+  for (var i = 0; i < 4; i++) {
+    var rowNumber = currentNode[0] + direction[i][0];
+    var colNumber = currentNode[1] + direction[i][1];
+
     if (isNotBlocked(rowNumber, colNumber, size) && isValid(arr, rowNumber, colNumber)) {
-      if (arr[rowNumber][colNumber] == "E") {
-        arr[currentPoint[0]][currentPoint[1]] = "1";
-        return true;
+      result = false;
+    }
+    if (arr[currentNode[0]][currentNode[1]] == "E") {
+      result = false;
+      i = Number.MAX_VALUE;
+    }
+  }
+  return result;
+}
+
+function FindFirstCoincidenceInMatrix(arr, symbol = "S") {
+  for (let row = 0; row < arr.length; row++) {
+    for (let col = 0; col < arr[row].length; col++) {
+      if (arr[row][col] == symbol) {
+        return [row, col];
       }
     }
   }
-
-  return false;
-}
-
-function SearchFreePath(arr, currentPoint, queueForCheaking, size) {
-  const direction = [
-    [0, 1], [0, -1],
-    [1, 0], [-1, 0],
-  ];
-  const pointSidesNumber = 4;
-  const oldQueue = queueForCheaking.length;
-
-  for (var i = 0; i < pointSidesNumber; i++) {
-    var rowNumber = currentPoint[0] + direction[i][0];
-    var colNumber = currentPoint[1] + direction[i][1];
-
-    if (isNotBlocked(rowNumber, colNumber, size) && isValid(arr, rowNumber, colNumber)) {
-      if (isDoor(arr, [rowNumber, colNumber], size))
-        return false;
-      queueForCheaking.push([rowNumber, colNumber]);
-    }
-  }
-
-  if (oldQueue == queueForCheaking.length) {
-    deadlockHandling(arr, currentPoint, size);
-    return queueForCheaking = [];
-  }
-
-  return queueForCheaking;
-}
-
-
-function deadlockHandling(arr, currentPoint, size) {
-  arr[currentPoint[0]][currentPoint[1]] = '#';
-  const clearArray = ClearVisited(arr, size);
-  SearchPath(clearArray, size);
 }
 
 function isNotBlocked(rowNumber, colNumber, size) {
@@ -148,26 +113,4 @@ function isValid(arr, rowNumber, colNumber, stopSymbols = ["#", "S", "1"]) {
     arr[rowNumber][colNumber] != startPoint &&
     arr[rowNumber][colNumber] != visiterPoint &&
     arr[rowNumber][colNumber] != EndPoint
-}
-
-function ClearVisited(arr, size) {
-  const [rowSize, colSize] = size;
-  for (let row = 0; row < rowSize; row++) {
-    for (let col = 0; col < colSize; col++) {
-      if (arr[row][col] == '1') {
-        arr[row][col] = '.'
-      }
-    }
-  }
-  return arr;
-}
-
-function FindFirstCoincidenceInMatrix(arr, symbol = "S") {
-  for (let row = 0; row < arr.length; row++) {
-    for (let col = 0; col < arr[row].length; col++) {
-      if (arr[row][col] == symbol) {
-        return [row, col];
-      }
-    }
-  }
 }
