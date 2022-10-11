aa45ceee2b128bd1340afa8da2c8defebe58ed02
Talerchik Alexey <talerchik@protonmail.com>
2022-10-11 11:52:29 +0300

    add: new algorithm

--- 1-and-0/index.js
+++ 1-and-0/index.js
@@ -1,36 +1,50 @@
-var score = -1;
+	var MOD = 1000000007;
+	
+	// Function to return the count
+	// of all possible valid strings
+	function countStrings(N)
+	{
+		var i, j;
+		
+		var dp = Array(N+1).fill(0).map(x => Array(3).fill(0));
+	
+		// Fill 0's in the dp array
+		for(i = 0; i < N + 1; i++)
+		{
+			for(j = 9; j < 3 ; j ++)
+			{
+				dp[i][j] = 0;
+			}
+		}
+		
+		// Base cases
+		dp[1][0] = 1;
+		dp[1][1] = 1;
+		dp[1][2] = 0;
+	
+		for (i = 2; i <= N; i++)
+		{
+	
+			// dp[i][j] = number of possible strings
+			// such that '1' just appeared consecutively
+			// j times upto the ith index
+			dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] +
+						dp[i - 1][2]) % MOD;
+	
+			// Taking previously calculated value
+			dp[i][1] = dp[i - 1][0] % MOD;
+			dp[i][2] = dp[i - 1][1] % MOD;
+		}
+	
+		// Taking all possible cases that
+		// can appear at the Nth position
+		var ans = (dp[N][0] + dp[N][1] +
+							dp[N][2]) % MOD;
+        console.log(dp);
+		return ans;
+	}
+	
+// Driver code
+var N = 10000000;
+console.log(countStrings(N));
 
-function ZeroOneGen(dataArr, digits, index,) {
-    var i, z;
-    if (index == digits) {
-        var str = "";
-
-        for (i = 0; i < digits; i++) {
-            str = str + dataArr[i];
-        }
-
-        if (!str.match(/111/)) {
-            score += 1;
-        }
-
-        return;
-    }
-
-    for (z = 0; z < 2; z++) {
-        dataArr[index] = z;
-        ZeroOneGen(dataArr, digits, index + 1);
-    }
-
-    return score;
-}
-
-const inputNumber = Number(prompt('Enter the number:'));
-
-try {
-    var dataArrTemp = new Array(inputNumber);
-    var resalt = ZeroOneGen(dataArrTemp, inputNumber, 0);
-
-    alert(resalt);
-} catch (e) {
-    alert(`${e.message}`)
-}
