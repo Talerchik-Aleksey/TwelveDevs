167b3a86c1b216422a253d7679bf257512cf6873
Talerchik Alexey <talerchik@protonmail.com>
2022-10-12 19:27:31 +0300

    fix

--- 1-and-0/index.js
+++ 1-and-0/index.js
@@ -1,50 +1,34 @@
-	var MOD = 1000000007;
-	
-	// Function to return the count
-	// of all possible valid strings
-	function countStrings(N)
-	{
-		var i, j;
-		
-		var dp = Array(N+1).fill(0).map(x => Array(3).fill(0));
-	
-		// Fill 0's in the dp array
-		for(i = 0; i < N + 1; i++)
-		{
-			for(j = 9; j < 3 ; j ++)
-			{
-				dp[i][j] = 0;
-			}
+const MOD = 1000_000_007;
+
+function countStrings(N) {
+	var i, j;
+
+	var array = Array(N + 1).fill(0).map(x => Array(3).fill(0));
+
+	for (i = 0; i < N + 1; i++) {
+		for (j = 0; j < 3; j++) {
+			array[i][j] = 0;
 		}
-		
-		// Base cases
-		dp[1][0] = 1;
-		dp[1][1] = 1;
-		dp[1][2] = 0;
-	
-		for (i = 2; i <= N; i++)
-		{
-	
-			// dp[i][j] = number of possible strings
-			// such that '1' just appeared consecutively
-			// j times upto the ith index
-			dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] +
-						dp[i - 1][2]) % MOD;
-	
-			// Taking previously calculated value
-			dp[i][1] = dp[i - 1][0] % MOD;
-			dp[i][2] = dp[i - 1][1] % MOD;
-		}
-	
-		// Taking all possible cases that
-		// can appear at the Nth position
-		var ans = (dp[N][0] + dp[N][1] +
-							dp[N][2]) % MOD;
-        console.log(dp);
-		return ans;
 	}
-	
-// Driver code
-var N = 10000000;
+
+	array[1][0] = 1;
+	array[1][1] = 1;
+	array[1][2] = 0;
+
+	for (i = 2; i <= N; i++) {
+		array[i][0] = (array[i - 1][0] + array[i - 1][1] +
+			array[i - 1][2]) % MOD;
+
+		array[i][1] = array[i - 1][0] % MOD;
+		array[i][2] = array[i - 1][1] % MOD;
+	}
+
+	var ans = (array[N][0] + array[N][1] +
+		array[N][2]) % MOD;
+
+	return ans - 1;
+}
+
+var N = 10_000_000; // 6 seconds
 console.log(countStrings(N));
 
