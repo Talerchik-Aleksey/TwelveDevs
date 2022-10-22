24ddfff1e06affd59d4466566954df17038fea18
Talerchik Alexey <talerchik@protonmail.com>
2022-10-22 13:29:27 +0300

    new: formula to create array of solution

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,36 +1,52 @@
 // const num = 91; // 30 seconds
+// Current index / past index * 1.2 * number of operation past index
 
-for (let index = 0; index < 32; index++) {
-    var count = 0;
 
-    console.log("------------------------");
-    getNumberOfOperation(index);
-    console.log(count + " - " + index);
-    console.log("------------------------");
-} 
+let array = CalculateMathSolution();
 
-var queue = [];
+console.log(array);
 
-function getNumberOfOperation(num, startNum = 1,) {
-    operation = [2, 5, 3];
 
-    if(num === 1) {
-        return;
+
+function CalculateMathSolution(to = 9_999_999) {
+    let solutionArray = [];
+    solutionArray[19] = 33;
+    for (let index = 20; index < to; index++) {
+        solutionArray.push((index / (index - 1)) * 1.15 * solutionArray[index - 1]);
     }
 
-    if (startNum === num) {
-        count += 1;
-    }
-    for (let index = 0; index < operation.length; index++) {
-        const element = operation[index];
-        if (startNum < num) {
-            if (index < 2 && startNum + element <= num) {
-                console.log(`+ ${element} | ${startNum}`)
-                getNumberOfOperation(num, startNum + element);
-            } else if (index === 2 && startNum * element <= num) {
-                console.log(`* ${element} | ${startNum}`)
-                getNumberOfOperation(num, startNum * element);
-            }
-        }
-    }
+    return solutionArray;
 }
+
+// for (let index = 0; index < 32; index++) {
+//     var count = 0;
+
+//     console.log("------------------------");
+//     getNumberOfOperation(index);
+//     console.log(count + " - " + index);
+//     console.log("------------------------");
+// }
+
+// function getNumberOfOperation(num, startNum = 1,) {
+//     operation = [2, 5, 3];
+
+//     if(num === 1) {
+//         return;
+//     }
+
+//     if (startNum === num) {
+//         count += 1;
+//     }
+//     for (let index = 0; index < operation.length; index++) {
+//         const element = operation[index];
+//         if (startNum < num) {
+//             if (index < 2 && startNum + element <= num) {
+//                 console.log(`+ ${element} | ${startNum}`)
+//                 getNumberOfOperation(num, startNum + element);
+//             } else if (index === 2 && startNum * element <= num) {
+//                 console.log(`* ${element} | ${startNum}`)
+//                 getNumberOfOperation(num, startNum * element);
+//             }
+//         }
+//     }
+// }
